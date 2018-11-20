c49c8749501f13131d64b8ea457fbd12d23c803b
Enzojz <enzojz@gmail.com>
2018-11-20 15:36:38 +0100

    Finished

--- luadump.lua
+++ luadump.lua
@@ -1,4 +1,34 @@
+-- MIT License
+
+-- Copyright (c) 2018 Enzojz
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+-- https://github.com/Enzojz/luadump
+-- Last update: 2018/11/20
+
 local tostring = tostring
+local floor = math.floor
+local debug = debug
+local table = table
+local insert = table.insert
+local sort = table.sort
 
 local function rawpairs(t)
     return next, t, nil
@@ -8,11 +38,9 @@ local function isSequenceKey(k, sequenceLength)
     return type(k) == 'number'
         and 1 <= k
         and k <= sequenceLength
-        and math.floor(k) == k
+        and floor(k) == k
 end
 
--- For implementation reasons, the behavior of rawlen & # is "undefined" when
--- tables aren't pure sequences. So we implement our own # operator.
 local function getSequenceLength(t)
     local len = 1
     local v = rawget(t, len)
@@ -32,48 +60,46 @@ local function getNonSequentialKeys(t)
             keys[keysLength] = k
         end
     end
-    table.sort(keys, sortKeys)
+    sort(keys, sortKeys)
     return keys, keysLength, sequenceLength
 end
 
 -- About from inspect.lua
-
-local function getlocals(l)
-  local i = 0
-  local direction = 1
-  return function ()
-    i = i + direction
-    local k,v = debug.getlocal(l,i)
-    if (direction == 1 and (k == nil or k.sub(k,1,1) == '(')) then 
-      i = -1 
-      direction = -1 
-      k,v = debug.getlocal(l,i) 
+local function getLocals(l)
+    local i = 0
+    local direction = 1
+    return function()
+        i = i + direction
+        local k, v = debug.getlocal(l, i)
+        if (direction == 1 and (k == nil or k.sub(k, 1, 1) == '(')) then
+            i = -1
+            direction = -1
+            k, v = debug.getlocal(l, i)
+        end
+        return k, v
     end
-    return k,v
-  end
 end
 
-local function dumpsig(f)
-  assert(type(f) == 'function', 
-    "bad argument #1 to 'dumpsig' (function expected)")
-  local p = {}
-  pcall (function() 
-    local oldhook
-    local hook = function(event, line)
-      for k,v in getlocals(3) do 
-        if k == "(*vararg)" then 
-          table.insert(p,"...") 
-          break
-        end 
-        table.insert(p,k) end
-      debug.sethook(oldhook)
-      error('aborting the call')
-    end
-    oldhook = debug.sethook(hook, "c")
-    -- To test for vararg must pass a least one vararg parameter
-    f(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
-  end)
-  return "function("..table.concat(p,",")..")"  
+local function dumpFn(f)
+    local params = {}
+    pcall(function()
+        local oldhook
+        local hook = function(event, line)
+            for k, _ in getLocals(3) do
+                if k == "(*vararg)" then
+                    insert(params, "...")
+                    break
+                end
+                insert(params, k)
+            end
+            debug.sethook(oldhook)
+            error('aborting the call')
+        end
+        oldhook = debug.sethook(hook, "c")
+        -- To test for vararg must pass a least one vararg parameter
+        f(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+    end)
+    return "(" .. table.concat(params, ", ") .. ")"
 end
 
 --Above from
@@ -95,104 +121,160 @@ local dumpKey = function(k)
     end
 end
 
-local function dump(isFnMute, fnSig)
-  local function printLua(node, indent)
-      local indent = indent or 0
-      local sindent = aIndent:rep(indent)
-      local callback = {
-          number = function() return tostring(node) end,
-          boolean = function() return node and "true" or "false" end,
-          string = function() return '"' .. node:format("%q") .. '"' end,
-          table = function()
-              local nonSequentialKeys, nonSequentialKeysLength, sequenceLength = getNonSequentialKeys(node)
-              
-              local simpleChildrenSeq = true
-              local simpleChildrenNSeq = true
-              
-              local fnList = {}
-              local seqStr = {}
-              for i = 1, sequenceLength do
-                  if (type(node[i]) == "function") then
-                      fnList[#fnList + 1] = i
-                  else
-                      seqStr[#seqStr + 1] = printLua(node[i], indent + 1)
-                      simpleChildrenSeq = simpleChildrenSeq and isSimple(node[i])
-                  end
-              end
-              
-              local nSeqStr = {}
-              local nSeqKeyMax = 0
-              for i = 1, nonSequentialKeysLength do
-                  local k = nonSequentialKeys[i]
-                  if (type(node[k]) == "function") then
-                      fnList[#fnList + 1] = k
-                  else
-                      local key = dumpKey(k)
-                      nSeqStr[#nSeqStr + 1] = {k = key, t = printLua(node[k], indent + 1)}
-                      simpleChildrenNSeq = simpleChildrenNSeq and isSimple(node[k])
-                      nSeqKeyMax = nSeqKeyMax > key:len() and nSeqKeyMax or key:len()
-                  end
-              end
-              
-              local str = ""
-              if (simpleChildrenSeq) then
-                  for i = 1, #seqStr do
-                      str = str .. seqStr[i] .. (i == #seqStr and "" or ", ")
-                  end
-              else
-                  for i = 1, #seqStr do
-                      str = str .. sindent .. seqStr[i] .. (i == #seqStr and "" or ",\n")
-                  end
-              end
-              
-              if (simpleChildrenNSeq) then
-                  local totalLength = 0
-                  for i = 1, #nSeqStr do
-                      totalLength = totalLength + #nSeqStr[i].t + #nSeqStr[i].k
-                  end
-                  if (totalLength < 120) then
-                      for i = 1, #nSeqStr do
-                          str = string.format("%s%s = %s%s", str, nSeqStr[i].k, nSeqStr[i].t, i == #nSeqStr and "" or ", ")
-                      end
-                  else
-                      simpleChildrenNSeq = false
-                      for i = 1, #nSeqStr do
-                          str = string.format("%s%s%s%s = %s%s", str, sindent,
-                              nSeqStr[i].k,
-                              string.rep(" ", nSeqKeyMax - #nSeqStr[i].k),
-                              nSeqStr[i].t,
-                              i == #nSeqStr and "" or ",\n"
-                      )
-                      end
-                  end
-              else
-                  for i = 1, #nSeqStr do
-                      str = string.format("%s%s%s = %s%s", str, sindent,
-                          nSeqStr[i].k,
-                          nSeqStr[i].t,
-                          i == #nSeqStr and "" or ",\n"
-                  )
-                  end
-              end
-              
-              return (simpleChildrenSeq and simpleChildrenNSeq) and string.format("{ %s }", str) or string.format("{\n%s\n%s}", str, aIndent:rep(indent - 1))
-          end,
-          ["function"] = function() return "" end,
-          ["userdata"] = function() return "" end,
-          ["thread"] = function() return "" end,
-          ["nil"] = function() return "" end
-      }
-      return callback[type(node)](node)
-  end
-  return printLua
+local function dump(printFn)
+    local function printLua(node, indent)
+        local indent = indent or 0
+        local sindent = aIndent:rep(indent + 1)
+        local callback = {
+            number = function() return tostring(node) end,
+            boolean = function() return node and "true" or "false" end,
+            string = function() return '"' .. node:format("%q") .. '"' end,
+            table = function()
+                local nonSequentialKeys, nonSequentialKeysLength, sequenceLength = getNonSequentialKeys(node)
+                
+                local simpleChildrenSeq = true
+                local simpleChildrenNSeq = true
+                
+                local seqIndex = {}
+                local seqFnIndex = {}
+                for i = 1, sequenceLength do
+                    if (type(node[i]) == "function") then
+                        if (printFn) then
+                            insert(seqIndex, i)
+                        else
+                            simpleChildrenSeq = false
+                        end
+                    else
+                        insert(seqIndex, i)
+                        simpleChildrenSeq = simpleChildrenSeq and isSimple(node[i])
+                    end
+                end
+                
+                local nSeqStr = {}
+                local nSeqFnStr = {}
+                local nSeqKeyMax = 0
+                local nSeqFnKeyMax = 0
+                for i = 1, nonSequentialKeysLength do
+                    local k = nonSequentialKeys[i]
+                    if (type(node[k]) == "function") then
+                        if (printFn) then
+                            local key = dumpKey(k)
+                            insert(nSeqFnStr, {k = key, t = printLua(node[k], indent + 1)})
+                            nSeqFnKeyMax = nSeqFnKeyMax > key:len() and nSeqFnKeyMax or key:len()
+                        end
+                    else
+                        local key = dumpKey(k)
+                        insert(nSeqStr, {k = key, t = printLua(node[k], indent + 1)})
+                        simpleChildrenNSeq = simpleChildrenNSeq and isSimple(node[k])
+                        nSeqKeyMax = nSeqKeyMax > key:len() and nSeqKeyMax or key:len()
+                    end
+                end
+                
+                local strSeq = ""
+                local strNSeq = ""
+                local strNSeqFn = ""
+                if (#seqIndex == sequenceLength) then
+                    if (simpleChildrenSeq) then
+                        for i = 1, #seqIndex do
+                            strSeq = string.format("%s%s%s",
+                                strSeq,
+                                printLua(node[seqIndex[i]], indent + 1),
+                                i == #seqIndex and (#nSeqStr == 0 and #nSeqFnStr == 0 and "" or ",\n") or ", "
+                        )
+                        end
+                    else
+                        for i = 1, #seqIndex do
+                            strSeq = string.format("%s%s%s%s",
+                                strSeq,
+                                sindent,
+                                printLua(node[seqIndex[i]], indent + 1),
+                                i == #seqIndex and #nSeqStr == 0 and #nSeqFnStr == 0 and "" or ",\n"
+                        )
+                        end
+                    end
+                else
+                    for i = 1, #seqIndex do
+                        strSeq = string.format("%s%s[%d] = %s%s",
+                            strSeq,
+                            sindent,
+                            seqIndex[i],
+                            printLua(node[seqIndex[i]], indent + 1),
+                            i == #seqIndex and #nSeqStr == 0 and #nSeqFnStr == 0 and "" or ",\n"
+                    )
+                    end
+                end
+                
+                if (simpleChildrenNSeq) then
+                    local totalLength = 0
+                    for i = 1, #nSeqStr do
+                        totalLength = totalLength + #nSeqStr[i].t + #nSeqStr[i].k
+                    end
+                    if (totalLength < 120) then
+                        for i = 1, #nSeqStr do
+                            strNSeq = string.format("%s%s = %s%s", strNSeq, nSeqStr[i].k, nSeqStr[i].t, i == #nSeqStr and (#nSeqFnStr == 0 and "" or ",\n") or ", ")
+                        end
+                    else
+                        simpleChildrenNSeq = false
+                        for i = 1, #nSeqStr do
+                            strNSeq = string.format("%s%s%s%s = %s%s",
+                                strNSeq,
+                                sindent,
+                                nSeqStr[i].k,
+                                string.rep(" ", nSeqKeyMax - #nSeqStr[i].k),
+                                nSeqStr[i].t,
+                                i == #nSeqStr and #nSeqFnStr == 0 and "" or ",\n"
+                        )
+                        end
+                    end
+                else
+                    for i = 1, #nSeqStr do
+                        strNSeq = string.format("%s%s%s = %s%s",
+                            strNSeq,
+                            sindent,
+                            nSeqStr[i].k,
+                            nSeqStr[i].t,
+                            i == #nSeqStr and #nSeqFnStr and "" or ",\n"
+                    )
+                    end
+                end
+                
+                for i = 1, #nSeqFnStr do
+                    strNSeqFn = string.format("%s%s%s = %s%s", strNSeqFn, sindent, nSeqFnStr[i].k, nSeqFnStr[i].t, i == #nSeqFnStr and "" or ",\n")
+                end
+                
+                if (simpleChildrenSeq and simpleChildrenNSeq) then
+                    if (strSeq:len() == 0 or strNSeq:len() == 0) and strNSeqFn:len() == 0 then
+                        return string.format("{ %s%s }", strSeq, strNSeq)
+                    else
+                        return string.format("{\n%s%s%s\n%s}",
+                            strSeq:len() > 0 and sindent .. strSeq or strSeq,
+                            strNSeq:len() > 0 and sindent .. strNSeq or strNSeq,
+                            strNSeqFn,
+                            aIndent:rep(indent))
+                    end
+                else
+                    return string.format("{\n%s%s%s\n%s}",
+                        simpleChildrenSeq and strSeq:len() > 0 and sindent .. strSeq or strSeq,
+                        simpleChildrenNSeq and strNSeq:len() > 0 and sindent .. strNSeq or strNSeq,
+                        strNSeqFn,
+                        aIndent:rep(indent))
+                end
+            end,
+            ["function"] = function() return dumpFn(node) end,
+            ["userdata"] = function() return "" end,
+            ["thread"] = function() return "" end
+        }
+        return callback[type(node)](node)
+    end
+    return printLua
 end
 
-return function(isFnMute, fnSig)
-  return function(...)
-      local args = {...}
-      for i = 1, #args do
-        print(dump(isFnMute or true, fnSig or false)(args[i]))
-      end
-      return ...
-  end
+return function(printFn)
+    return function(...)
+        local args = {...}
+        for i = 1, #args do
+            print(dump(printFn or false)(args[i]))
+        end
+        return ...
+    end
 end
